e723b206bc0039072eda2147373ca05817a5614c
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-09-01 19:35:12 +0700

    Creating Project files

---
+++ src/components/FormSignUp/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+const SignUp = () => {
+	return (
+		<div>
+			<h2>SignUp</h2>
+		</div>
+	);
+};
+
+export default SignUp;
---
+++ src/components/FormSuccess/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+const FormSuccess = () => {
+	return (
+		<div>
+			<h2>FormSuccess</h2>
+		</div>
+	);
+};
+
+export default FormSuccess;
